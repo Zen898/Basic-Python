fee9ef5b38a5c28bab1811c55f14da92649385dd
Zen898 <syouzenyuki@yahoo.co.jp>
2023-05-12 23:57:22 +0900

    solved problems

--- euclid.py
+++ euclid.py
@@ -3,13 +3,45 @@ b = input("b の値を入力: ")
 
 # TODO
 
-if b > a:
-    (a, b) = (b, a)
+# if b > a:
+#     (a, b) = (b, a)
 
-a = int(a)
-b = int(b)
+# a = int(a)
+# b = int(b)
 
-while b != 0:
-    (a, b) = (b, a % b)
+# while b != 0:
+#     (a, b) = (b, a % b)
 
-print(a)
+# print(a)
+
+# 問3
+
+
+def euclid(a, b):
+    if b > a:
+        (a, b) = (b, a)
+
+    a = int(a)
+    b = int(b)
+
+    while b != 0:
+        (a, b) = (b, a % b)
+
+    print(f"最大公約数は{a}")
+    return a
+
+
+# euclid(a, b)
+
+
+# 問4
+
+
+def prime_euclid(a, b):
+    if euclid(a, b) == 1:
+        print(f"従って{a}と{b}は互いに素です")
+    else:
+        print(f"従って{a}と{b}は互いに素ではありません")
+
+
+prime_euclid(a, b)
--- prime_number.py
+++ prime_number.py
@@ -1,21 +1,41 @@
+# a = input("aの値を入力: ")
+# b = input("bの値を入力: ")
+
+# # TODO
+
+# a = int(a)
+# b = int(b)
+
+# for i in range(2, a):
+#     if a % i == 0:
+#         print(f"{a}は素数ではありません")
+#         break
+# else:
+#     print(f"{a}は素数です")
+
+# for i in range(2, b):
+#     if b % i == 0:
+#         print(f"{b}は素数ではありません")
+#         break
+# else:
+#     print(f"{b}は素数です")
+
+def prime_num(n):
+    list_n = list(n)
+    if "." in list_n or "-" in list_n:
+        print(f"{n}は自然数ではありません")
+        return False
+    else:
+        n = int(n)
+        for i in range(2, n):
+            if n % i == 0:
+                print(f"{n}は素数ではありません")
+                return False
+        else:
+            print(f"{n}は素数です")
+            return True
+
+
 a = input("aの値を入力: ")
-b = input("bの値を入力: ")
 
-# TODO
-
-a = int(a)
-b = int(b)
-
-for i in range(2, a):
-    if a % i == 0:
-        print(f"{a}は素数ではありません")
-        break
-else:
-    print(f"{a}は素数です")
-
-for i in range(2, b):
-    if b % i == 0:
-        print(f"{b}は素数ではありません")
-        break
-else:
-    print(f"{b}は素数です")
+prime_num(a)
